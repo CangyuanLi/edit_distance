71c6383438765638025f67d003b0d87449d0925d
CangyuanLi <everest229@gmail.com>
2023-03-23 04:58:10 -0400

    parallelize tests

--- tests/test_fast_distance.py
+++ tests/test_fast_distance.py
@@ -1,32 +1,43 @@
-import fast_distance
-import jellyfish
+import concurrent.futures
 import random
 import string
 
-def build_string(min: int=1, max: int=1_000):
-    k = random.randint(min, max)
+import jellyfish
+import pytest
 
-    return "".join(random.choices(string.printable, k=k))
+import fast_distance
 
 def build_ascii_string(min: int=1, max: int=1_000):
     k = random.randint(min, max)
 
-    return "".join(random.choices(string.ascii_letters, k=k))
+    return "".join(random.choices(string.printable, k=k))
 
-def test_hamming():
-    lst1 = [build_string() for _ in range(100_000)]
-    lst2 = [build_string() for _ in range(100_000)]
-    for s1, s2 in zip(lst1, lst2):
+@pytest.fixture
+def lst1():
+    return [build_ascii_string() for _ in range(100_000)]
+
+@pytest.fixture
+def lst2():
+    return [build_ascii_string() for _ in range(100_000)]
+
+def test_hamming(lst1, lst2):
+    def compare(strs: tuple[str, str]):
+        s1, s2 = strs
         res1 = fast_distance.hamming_distance(s1, s2)
         res2 = jellyfish.hamming_distance(s1, s2)
 
         assert res1 == res2
 
-def test_damerau_levenshtein():
-    lst1 = [build_string() for _ in range(100_000)]
-    lst2 = [build_string() for _ in range(100_000)]
-    for s1, s2 in zip(lst1, lst2):
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        pool.map(compare, zip(*lst1, *lst2))
+
+def test_damerau_levenshtein(lst1, lst2):
+    def compare(strs: tuple[str, str]):
+        s1, s2 = strs
         res1 = fast_distance.damerau_levenshtein_distance(s1, s2)
         res2 = jellyfish.damerau_levenshtein_distance(s1, s2)
 
         assert res1 == res2
+
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        pool.map(compare, zip(*lst1, *lst2))
